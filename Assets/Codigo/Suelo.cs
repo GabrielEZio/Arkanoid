e39b7c81812d91fb9a80ad777dd5d5371019bf6b
GabrielEZio <eiraldi.gabriel3@gmail.com>
2018-02-07 20:22:50 +0100

    Contador de vidas y destruir bloques

---
+++ Assets/Codigo/Suelo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suelo : MonoBehaviour {
+
+    public Vidas vidasJugador;
+
+     void OnTriggerEnter( )
+    {
+        vidasJugador.PerderVida();
+    }
+}
---
+++ Assets/Codigo/Vidas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vidas : MonoBehaviour {
+
+    public static int vidas = 3;
+    public Text textoVidas;
+    public Pelota pelota;
+    public Barra barra;
+
+	// Use this for initialization
+	void Start () {
+        textoVidas.text = "Vidas: " + Vidas.vidas;
+	}
+	
+	// Update is called once per frame
+	public void PerderVida()
+    {
+        Vidas.vidas--;
+        textoVidas.text = "Vidas: " + Vidas.vidas;
+        barra.Reset();
+        pelota.Reset();
+       
+
+            
+    }
+}
